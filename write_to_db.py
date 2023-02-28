6d498df24a561891a253b70c30c0cd44aadf3db7
Ayush Dhananjai <ayushdhananjai@Ayushs-MacBook-Pro-2.local>
2023-02-28 15:09:05 -0500

    added logic to python script that updates the repository names for all users if the user exists. If it doesn't exist, it creates a new entry in the collection

--- write_to_db.py
+++ write_to_db.py
@@ -6,8 +6,8 @@ import argparse
 
 
 class WriteToDatabase:
-
     """ constructor """
+
     def __init__(self, all_usernames_string):
         # create a client to connect to the database
         self.client = MongoClient(
@@ -21,48 +21,54 @@ class WriteToDatabase:
         self.collection = self.db.user_repos
 
         # parse the usernames
-        self.usernames = self._parse_usernames(all_usernames_string)
-        print(self.usernames)
+        self.usernames = all_usernames_string.split(",")
 
-    """ we want to split the input as it is a comma separated input """
-    def _parse_usernames(self, usernames):
-        split_usernames = usernames.split(",")
-        actual_usernames = []
-        for username in split_usernames:
-            result = self.collection.find_one({"user_name": username})
-            if result == None:
-                actual_usernames.append(username)
-        return actual_usernames
+        # The base URL for the API that extracts the repositories for a particular user
+        self.base_github_api_url = "https://api.github.com/users/"
 
     """ populate the database with the names of all the repositories"""
-    def populate_database(self):
-        # loop over all the names in our global array
 
+    def populate_database(self):
+
+        # loop over all the names in our username array
         for i in range(len(self.usernames)):
+            # extract the current username and update the endpoint url
             current_username = self.usernames[i]
-            url = "https://api.github.com/users/" + current_username + "/repos"
+            url = self.base_github_api_url + current_username + "/repos"
+
             try:
                 response = requests.get(url)
+                # if we get a successful response
                 if response.status_code == 200:
-                    repos = [repo['name'] for repo in response.json()]
-                    current_user_data = {'user_name': current_username, 'all_repo_names': repos}
-                    self.collection.insert_one(current_user_data)
-                    print("Inserted data for username = " + current_username)
+
+                    # extract all the repository names for this user and save the results in a list
+                    repositories = [repository['name'] for repository in response.json()]
+
+                    # Here we are checking to see if the data doesn't exist in the database. If it doesn't exist,
+                    # then it is a new entry in our collection. Else, we must update the repositories list for the
+                    # existing user
+                    if self.collection.find_one({"user_name": current_username}) is None:
+                        current_user_data = {'user_name': current_username, 'all_repo_names': repositories}
+                        self.collection.insert_one(current_user_data)
+                        print("Inserted new data for username = " + current_username)
+
+                    else:
+                        # update the existing username's "all_repo_names" field with the new set of repositories.
+                        new_repository_names = {"$set": {'all_repo_names': repositories}}
+                        self.collection.update_one({"user_name": current_username}, new_repository_names)
+                        print("Updated repository names for username = " + current_username)
                 else:
                     print("Unable to insert data for: " + current_username)
-                    return
             except Exception as e:
-                print("an error occurred when trying to insert data. Please try again.")
-
-
-
-
+                print("An error occurred when trying to insert data. Please try again.")
 
 
 def main(args):
+    # create an instance of the class
     database_instance = WriteToDatabase(args)
-    database_instance.populate_database()
 
+    # populate the database with the repository names for all users, old and new
+    database_instance.populate_database()
 
 
 if __name__ == '__main__':
